6a657b92a20648793adf19c001e93b44cde7b035
revin de castro <redc1026@gmail.com>
2024-11-18 11:52:54 +0800

    Link react router dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Logo from "../assets/img/global_logo.png";
-
+import { Link } from 'react-router-dom';
 function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isSticky, setIsSticky] = useState(false);
@@ -22,9 +22,9 @@ function Navbar() {
             className={`font-[sans-serif] tracking-wide ${isSticky ? 'fixed top-0 left-0 right-0 z-50 bg-white bg-opacity-40 backdrop-blur-md' : 'relative'}`}>
             {/* Main navbar section */}
             <section className='flex flex-col lg:items-center lg:justify-center gap-4 py-3 sm:px-10 px-4 border-gray-200 border-b min-h-[75px]'>
-                <a href="/gepdc/home" className="shrink-0">
+                <Link to="/gepdc/home" className="shrink-0">
                     <img src={Logo} alt="logo" className='md:w-[110px] w-28' />
-                </a>
+                </Link>
             </section>
 
             {/* Mobile and desktop navigation */}
@@ -40,55 +40,55 @@ function Navbar() {
                         max-lg:overflow-auto z-50 ${isMenuOpen ? 'block' : 'hidden'}`}>
                         {/* Logo item in mobile menu */}
                         <li className='max-lg:border-b max-lg:pb-4 px-3 lg:hidden'>
-                            <a href="/gepdc/home">
+                            <Link to="/gepdc/home">
                                 <img src={Logo} alt="logo" className='w-36' />
-                            </a>
+                            </Link>
                         </li>
 
                         {/* About Section with dropdown */}
                         <li className='group max-lg:border-b max-lg:px-3 max-lg:py-3 relative'>
-                            <a href='/gepdc/about' className='hover:text-green-500 text-gray-700 font-semibold text-[15px] block'>
+                            <Link to='/gepdc/about' className='hover:text-green-500 text-gray-700 font-semibold text-[15px] block'>
                                 About
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" className="ml-1 inline-block" viewBox="0 0 24 24">
                                     <path d="M12 16a1 1 0 0 1-.71-.29l-6-6a1 1 0 0 1 1.42-1.42l5.29 5.3 5.29-5.29a1 1 0 0 1 1.41 1.41l-6 6a1 1 0 0 1-.7.29z" />
                                 </svg>
-                            </a>
+                            </Link>
                             <ul className='absolute top-5 max-lg:top-8 left-0 z-50 rounded-xl block space-y-2 shadow-lg bg-white max-h-0 overflow-hidden min-w-[200px] group-hover:opacity-100 group-hover:max-h-[700px] px-6 group-hover:pb-4 group-hover:pt-6 transition-all duration-500'>
                                 <li className='border-b pb-2'>
-                                    <a href='/gepdc/about' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>About Our Company</a>
+                                    <Link to='/gepdc/about' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>About Our Company</Link>
                                 </li>
                                 <li className='border-b py-3'>
-                                    <a href='##' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Our Teams</a>
+                                    <Link to='/gepdc/teams' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Our Teams</Link>
                                 </li>
                                 <li className='pt-2'>
-                                    <a href='##' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Careers</a>
+                                    <Link to='/gepdc/careers' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Careers</Link>
                                 </li>
                             </ul>
                         </li>
 
                         {/* Projects Section with dropdown */}
                         <li className='group max-lg:border-b max-lg:px-3 max-lg:py-3 relative'>
-                            <a href='/gepdc/projects' className='hover:text-green-500 text-gray-700 font-semibold text-[15px] block'>
+                            <Link to='/gepdc/projects' className='hover:text-green-500 text-gray-700 font-semibold text-[15px] block'>
                                 Projects
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" className="ml-1 inline-block" viewBox="0 0 24 24">
                                     <path d="M12 16a1 1 0 0 1-.71-.29l-6-6a1 1 0 0 1 1.42-1.42l5.29 5.3 5.29-5.29a1 1 0 0 1 1.41 1.41l-6 6a1 1 0 0 1-.7.29z" />
                                 </svg>
-                            </a>
+                            </Link>
                             <ul className='absolute top-5 max-lg:top-8 left-0 z-50 block rounded-xl space-y-2 shadow-lg bg-white max-h-0 overflow-hidden min-w-[260px] group-hover:opacity-100 group-hover:max-h-[700px] px-6 group-hover:pb-4 group-hover:pt-6 transition-all duration-500'>
                                 <li className='border-b pb-2'>
-                                    <a href='/gepdc/projects/giga8' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>GIGA 8 Project</a>
+                                    <Link to='/gepdc/projects/giga8' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>GIGA 8 Project</Link>
                                 </li>
                                 <li className='border-b py-3'>
-                                    <a href='/gepdc/projects/sspc' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>SSPC Project</a>
+                                    <Link to='/gepdc/projects/sspc' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>SSPC Project</Link>
                                 </li>
                                 <li className='border-b py-3'>
-                                    <a href='/gepdc/projects/dspp' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>DSPP Project</a>
+                                    <Link to='/gepdc/projects/dspp' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>DSPP Project</Link>
                                 </li>
                                 <li className='pt-2'>
-                                    <a href='/gepdc/projects/tera3' className='text-gray-400 font-semibold text-[15px] block cursor-not-allowed' aria-disabled='true'>
+                                    <Link to='/gepdc/projects/tera3' className='text-gray-400 font-semibold text-[15px] block cursor-not-allowed' aria-disabled='true'>
                                         TERA 3 Project
                                         <span className='ml-1 text-gray-500 italic text-xs'>(Coming Soon)</span>
-                                    </a>
+                                    </Link>
                                 </li>
                             </ul>
                         </li>
@@ -103,29 +103,29 @@ function Navbar() {
                             </button>
                             <ul className='absolute top-5 max-lg:top-8 left-0 z-50 block rounded-xl space-y-2 shadow-lg bg-white max-h-0 overflow-hidden min-w-[250px] group-hover:opacity-100 group-hover:max-h-[700px] px-6 group-hover:pb-4 group-hover:pt-6 transition-all duration-500'>
                                 <li className='border-b pb-2'>
-                                    <a href='/gepdc/services/solar-plant' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Solar Power Plant</a>
+                                    <Link to='/gepdc/services/solar-plant' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Solar Power Plant</Link>
                                 </li>
                                 <li className='border-b py-3'>
-                                    <a href='/gepdc/services/thermal-scanning' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Thermal Scanning</a>
+                                    <Link to='/gepdc/services/thermal-scanning' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Thermal Scanning</Link>
                                 </li>
                                 <li className='border-b py-3'>
-                                    <a href='/gepdc/services/power-system' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Power Systems</a>
+                                    <Link to='/gepdc/services/power-system' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Power Systems</Link>
                                 </li>
                                 <li className='pt-2'>
-                                    <a href='/gepdc/services/electrical-testing' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Electrical Testing</a>
+                                    <Link to='/gepdc/services/electrical-testing' className='hover:text-green-500 text-gray-600 font-semibold text-[15px] block'>Electrical Testing</Link>
                                 </li>
                             </ul>
                         </li>
                         <li className='max-lg:border-b max-lg:px-3 max-lg:py-3'>
-                            <a href='/gepdc/clients' className='hover:text-green-500 text-gray-700 font-semibold text-[15px] block'>
+                            <Link to='/gepdc/clients' className='hover:text-green-500 text-gray-700 font-semibold text-[15px] block'>
                                 Clients
-                            </a>
+                            </Link>
                         </li>
                         {/* Contact Section */}
                         <li className='max-lg:border-b max-lg:px-3 max-lg:py-3'>
-                            <a href='/gepdc/contact' className='hover:text-green-500 text-gray-700 font-semibold text-[15px] block'>
+                            <Link to='/gepdc/contact' className='hover:text-green-500 text-gray-700 font-semibold text-[15px] block'>
                                 Contact
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
